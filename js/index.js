6629a86f191f3f0a7caa4cf4c175feb13990a618
BailLiu <110826274+BailLiu@users.noreply.github.com>
2025-05-29 19:52:00 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||端|午|安|康|#rectangle|');
+      S.UI.simulate('|#countdown 3||단오의|안녕을|기원합니다|#rectangle|');
     }
 
     S.Drawing.loop(function () {
